1c339dbd098f4154dcd42711a10be4b79e63569f
rafiasmaulrizal <rafiarizals@gmail.com>
2024-06-02 06:17:01 +0700

    pages

--- pages/index.php
+++ pages/index.php
@@ -152,14 +152,14 @@ $kategori = query("SELECT * FROM kategori");
   <!-- List Paket End -->
 
   <?php
-include '../templates/footer.php';
-?>
+  include '../templates/footer.php';
+  ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-j57Jd5rN2MdaT+qep0Pp3tYhkLgF3N3jozFWnSJ2MgjpjU6lC8k2x5z8FqY5KfHI" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-<script>
-  AOS.init();
-</script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-j57Jd5rN2MdaT+qep0Pp3tYhkLgF3N3jozFWnSJ2MgjpjU6lC8k2x5z8FqY5KfHI" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
 </body>
 
 </html>
